74eae8ab16a1f1fb5800b33f6b2ec63a4d343db1
Julien Atanassian <julien.atanassian@gmail.com>
2019-08-28 21:51:18 +0000

    Added the possibility to send a message

--- input.js
+++ input.js
@@ -21,6 +21,9 @@ const handleUserInput = function(key) {
   if (key === "\u0064") {
     connection.write("Move: right");
  }
+  if (key === "\u0061") {
+    connection.write("Say: Hello :)");
+ }
 };
 
 const setupInput = function(conn) {
