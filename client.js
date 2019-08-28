ab91784c9a3aaf3e17315c6fdd466da32a21ca12
Julien Atanassian <julien.atanassian@gmail.com>
2019-08-28 19:45:17 +0000

    Implemented events on connection with the server

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.0.102', // IP ADDRESS HERE
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', message => {
+    console.log(message);
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: JA")
+  });
+  
+  return conn;
+}
+
+module.exports = { connect };
